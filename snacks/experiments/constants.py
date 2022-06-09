4ef02a9635b8ad545afafccde8bb4ad65315af7d
Sofiane Tanji <sofiane.tanji@grenoble-inp.org>
2022-06-09 11:53:33 +0200

    hyperparameter updated for higgs

--- snacks/experiments/constants.py
+++ snacks/experiments/constants.py
@@ -2,39 +2,38 @@ BEST_VALUES = {}
 
 N_SAMPLES = {}
 
-BEST_VALUES["a9a"] = [500, 0.01, 1e-5, 1950000]
+BEST_VALUES["a9a"] = [550, 0.01, 1e-5, int(5e6)]
 N_SAMPLES["a9a"] = 32561
 
-BEST_VALUES["ijcnn1"] = [550, 2., 1e-8, 1950000]
+BEST_VALUES["ijcnn1"] = [500, 2., 1e-8, int(6e5)]
 N_SAMPLES["ijcnn1"] = 49990
 
-BEST_VALUES["SUSY"] = [330, 1e-2, 1e-7, 1950000]
+BEST_VALUES["SUSY"] = [330, 1e-2, 1e-7, int(5e6)]
 N_SAMPLES["SUSY"] = 5e6
 
-BEST_VALUES["HIGGS"] = [3000, 7e-6,  1e-8, 1950000]
+BEST_VALUES["HIGGS"] = [3000, 0.0034,  1e-8, 1050000]
 N_SAMPLES["HIGGS"] = 11e6
 
-BEST_VALUES["mnist"] = [20000, 0.005, 1e-8, 1950000]
+BEST_VALUES["mnist"] = [20000, 0.005, 1e-8, int(2e6)]
 N_SAMPLES["mnist"] = 60000
 
-BEST_VALUES["SVHN"] = [2000, 5.45, 1e-8, 1950000]
+BEST_VALUES["SVHN"] = [2000, 5.45, 1e-8, int(5e6)]
 N_SAMPLES["SVHN"] = 73257
 
-BEST_VALUES["epsilon"] = [3000, 7e-6, 1e-8, 1950000]
+BEST_VALUES["epsilon"] = [3000, 7e-6, 1e-8, 1050000]
 N_SAMPLES["epsilon"] = 400000
 
-BEST_VALUES["url"] = [1500, 5e-4, 3e-6, 1950000]
+BEST_VALUES["url"] = [1500, 5e-4, 3e-6, 1050000]
 N_SAMPLES["url"] = 2396130
 
-
 # not considered anymore
-BEST_VALUES["real-sim"] = [7500, 3e-1, 3e-6, 755000]
+BEST_VALUES["real-sim"] = [6000, 5e-1, 1e-7, 1050000]
 N_SAMPLES["real-sim"] = 72309
 
 BEST_VALUES["news20.binary"] = [5500, 5e-1, 1e-8, 245000]
 N_SAMPLES["news20.binary"] = 19996
 
-BEST_VALUES["covtype.binary"] = [3000, 7e-6,  1e-8, 1950000]
+BEST_VALUES["covtype.binary"] = [3000, 7e-6,  1e-8, 1050000]
 N_SAMPLES["covtype.binary"] = 581012
 
 BEST_VALUES["rcv1.binary_test"] = [1000, 5e-3, 1e-8, 90000]
